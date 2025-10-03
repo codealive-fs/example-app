695771cf53d61769abc0b983b54967c1a8a8f1cb
Faiz U. Shaikh <61307003+codealive-fs@users.noreply.github.com>
2025-10-03 12:56:13 +0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,7 +5,7 @@ cd /var/www/html
 # Ensure storage and cache directories exist
 mkdir -p storage bootstrap/cache
 
-
+sudo composer install --no-dev --optimize-autoloader
 
 # Set permissions (Ubuntu’s Apache user is www-data)
 sudo chown -R www-data:www-data storage bootstrap/cache
